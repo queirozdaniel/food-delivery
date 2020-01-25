d8ae68e2da91c553bed5ca1e3ad8f3ceee7a69e2
queirozdaniel <danielqueirozak77@gmail.com>
2020-01-25 20:52:40 -0300

    Adicionando Lombok para diminuir verbosidade de Modelos

---
+++ src/main/java/com/danielqueiroz/fooddelivery/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.danielqueiroz.fooddelivery.infrastructure.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.danielqueiroz.fooddelivery.domain.model.Cozinha;
+import com.danielqueiroz.fooddelivery.domain.repository.CozinhaRepository;
+
+@Repository
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+	
+}
