b6a365dcaca1ef7c16a227c9ccc5f3c2c4dbb4c0
queirozdaniel <danielqueirozak77@gmail.com>
2020-05-20 22:51:08 -0300

    Começando a implementação de DTOs

---
+++ src/main/java/com/danielqueiroz/fooddelivery/api/model/CozinhaDTO.java
@@ -0,0 +1,13 @@
+package com.danielqueiroz.fooddelivery.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CozinhaDTO {
+
+	private Long id;
+	private String nome;
+	
+}
