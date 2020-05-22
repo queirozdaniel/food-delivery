afe17d85730ad75c5d83e3791b68d96bb4037156
queirozdaniel <danielqueirozak77@gmail.com>
2020-05-21 22:21:12 -0300

    Refatorando metodo PUT de Restaurante para utilizar Mapper

--- src/main/java/com/danielqueiroz/fooddelivery/api/controller/RestauranteController.java
+++ src/main/java/com/danielqueiroz/fooddelivery/api/controller/RestauranteController.java
@@ -1,24 +1,14 @@
 package com.danielqueiroz.fooddelivery.api.controller;
 
-import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.SmartValidator;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,11 +21,8 @@ import com.danielqueiroz.fooddelivery.api.model.RestauranteDTO;
 import com.danielqueiroz.fooddelivery.api.model.assembler.RestauranteDTOAssembler;
 import com.danielqueiroz.fooddelivery.api.model.assembler.RestauranteInputDisassembler;
 import com.danielqueiroz.fooddelivery.api.model.input.RestauranteInput;
-import com.danielqueiroz.fooddelivery.core.validation.ValidacaoException;
 import com.danielqueiroz.fooddelivery.domain.model.Restaurante;
 import com.danielqueiroz.fooddelivery.domain.service.RestauranteService;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -43,9 +30,6 @@ public class RestauranteController {
 
 	@Autowired
 	private RestauranteService restauranteService;
-
-	@Autowired
-	private SmartValidator smartValidator;
 
 	@Autowired
 	private RestauranteDTOAssembler restauranteDTOAssembler;
@@ -76,12 +60,12 @@ public class RestauranteController {
 	@PutMapping("/{id}")
 	public RestauranteDTO atualizar(@RequestBody @Valid RestauranteInput restauranteInput, @PathVariable Long id) {
 
-		Restaurante restaurante = restauranteInputDisassembler.toDomainObject(restauranteInput);
-		
 		Restaurante restauranteRetornado = restauranteService.buscarPorId(id);
 
-		BeanUtils.copyProperties(restaurante, restauranteRetornado, "id", "formasPagamento", "endereco",
-				"dataCadastro");
+		restauranteInputDisassembler.copyToDomainObject(restauranteInput, restauranteRetornado);
+		
+//		BeanUtils.copyProperties(restaurante, restauranteRetornado, "id", "formasPagamento", "endereco",
+//				"dataCadastro");
 
 		return restauranteDTOAssembler.toModel(restauranteService.salvar(restauranteRetornado));
 
@@ -94,56 +78,5 @@ public class RestauranteController {
 		return ResponseEntity.noContent().build();
 	}
 
-	@PatchMapping("/{id}")
-	public ResponseEntity<?> atualizarParcial(@RequestBody Map<String, Object> campos, @PathVariable Long id,
-			HttpServletRequest request) {
-
-		Restaurante restauranteAtual = restauranteService.buscarPorId(id);
-		if (restauranteAtual == null) {
-			return ResponseEntity.notFound().build();
-		}
-
-		modificaCampos(campos, restauranteAtual, request);
-		validate(restauranteAtual, "restaurante");
-
-		return atualizar(restauranteAtual, id);
-	}
-
-	private void validate(Restaurante restaurante, String objectName) {
-		BeanPropertyBindingResult bindingResult = new BeanPropertyBindingResult(restaurante, objectName);
-
-		smartValidator.validate(restaurante, bindingResult);
-		if (bindingResult.hasErrors()) {
-			throw new ValidacaoException(bindingResult);
-		}
-
-	}
-
-	private void modificaCampos(Map<String, Object> campos, Restaurante restauranteAtual, HttpServletRequest request) {
-
-		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
-
-		// TODO: Criar um Bean para fazer o acesso aos valores
-		try {
-			ObjectMapper objectMapper = new ObjectMapper();
-			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
-
-			Restaurante restauranteOrigem = objectMapper.convertValue(campos, Restaurante.class);
-
-			campos.forEach((key, value) -> {
-				Field field = ReflectionUtils.findField(Restaurante.class, key);
-				field.setAccessible(true);
-
-				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-
-				ReflectionUtils.setField(field, restauranteAtual, novoValor);
-
-			});
-
-		} catch (IllegalArgumentException e) {
-			Throwable rootCause = e.getCause();
-			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
-		}
-	}
 
 }
--- src/main/java/com/danielqueiroz/fooddelivery/api/model/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/danielqueiroz/fooddelivery/api/model/assembler/RestauranteInputDisassembler.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.danielqueiroz.fooddelivery.api.model.input.RestauranteInput;
+import com.danielqueiroz.fooddelivery.domain.model.Cozinha;
 import com.danielqueiroz.fooddelivery.domain.model.Restaurante;
 
 @Component
@@ -15,6 +16,12 @@ public class RestauranteInputDisassembler {
 	
 	public Restaurante toDomainObject(RestauranteInput restauranteInput) {
 		return modelMapper.map(restauranteInput, Restaurante.class);
+	}
+	
+	public void copyToDomainObject(RestauranteInput restauranteInput, Restaurante restaurante) {
+		restaurante.setCozinha(new Cozinha());
+		
+		modelMapper.map(restauranteInput, restaurante);
 	}
 	
 }
