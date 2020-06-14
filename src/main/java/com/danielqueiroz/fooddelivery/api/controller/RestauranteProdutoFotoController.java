4c7dabcb73505b34c9e31eb4f56c3ab1ddd34ed8
queirozdaniel <danielqueirozak77@gmail.com>
2020-06-13 22:10:07 -0300

    Implementando envio de arquivos para servidor

---
+++ src/main/java/com/danielqueiroz/fooddelivery/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,37 @@
+package com.danielqueiroz.fooddelivery.api.controller;
+
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.danielqueiroz.fooddelivery.api.model.input.FotoProdutoInput;
+
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+	
+	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, FotoProdutoInput fotoProduto) {
+		
+		var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProduto.getArquivo().getOriginalFilename();
+		var arquivoFoto = Path.of("/Users/danie/Documents", nomeArquivo);
+
+		System.out.println(arquivoFoto);
+		System.out.println(fotoProduto.getArquivo().getContentType());
+		System.out.println(fotoProduto.getDescricao());
+		
+		try {
+			fotoProduto.getArquivo().transferTo(arquivoFoto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
---
+++ src/main/java/com/danielqueiroz/fooddelivery/api/model/input/FotoProdutoInput.java
@@ -0,0 +1,15 @@
+package com.danielqueiroz.fooddelivery.api.model.input;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FotoProdutoInput {
+
+	private MultipartFile arquivo;
+	private String descricao;
+	
+}
