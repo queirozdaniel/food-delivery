c71333b81d5d8cbe79f0e961e340531388986aef
queirozdaniel <danielqueirozak77@gmail.com>
2020-06-26 11:07:10 -0300

    Habilitando o CORS para comunicação com outras aplicações

--- src/main/java/com/danielqueiroz/fooddelivery/api/controller/RestauranteController.java
+++ src/main/java/com/danielqueiroz/fooddelivery/api/controller/RestauranteController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.danielqueiroz.fooddelivery.domain.model.Restaurante;
 import com.danielqueiroz.fooddelivery.domain.service.RestauranteService;
 import com.fasterxml.jackson.annotation.JsonView;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
