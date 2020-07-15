fa304076c083db3a9c6df5419c06736e233c53fd
queirozdaniel <danielqueirozak77@gmail.com>
2020-07-15 00:44:36 -0300

    Configurando autenticação em outro Webservice

--- src/main/java/com/danielqueiroz/fooddelivery/core/security/ResourceServerConfig.java
+++ src/main/java/com/danielqueiroz/fooddelivery/core/security/ResourceServerConfig.java
@@ -4,23 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
-				.and()
-				.authorizeRequests()
-					.anyRequest().authenticated()
-				.and()
-					.sessionManagement()
-						.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and()
-					.csrf().disable();
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.oauth2ResourceServer().opaqueToken();
 	}
+	
+	
 	
 }
